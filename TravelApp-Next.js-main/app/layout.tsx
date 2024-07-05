d1626a378b61efef9fc0c639c6796e1cc4f5b54c
JAY PRAKASH <97893456+IamJayPrakash@users.noreply.github.com>
2024-07-05 09:06:53 +0530

    Update layout.tsx

--- TravelApp-Next.js-main/app/layout.tsx
+++ TravelApp-Next.js-main/app/layout.tsx
@@ -5,8 +5,7 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "TrekEase",
-  description:
-    "TrekEase is all about making your treks effortless. Plan and explore the world with ease.",
+  description: "TrekEase is all about making your treks effortless. Plan and explore the world with ease.",
 };
 
 export default function RootLayout({
@@ -17,19 +16,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-adsense-account" content="ca-pub-9129390699994968">
-        <link rel="icon" href="/" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/path/to/your/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-9129390699994968",
+                enable_page_level_ads: true
+              });
+            `,
+          }}
+        />
       </head>
       <body>
         <Navbar />
-        <main className=" relative overflow-hidden">
-
+        <main className="relative overflow-hidden">
           {children}
-
         </main>
         <Footer />
-
-
       </body>
     </html>
   );
